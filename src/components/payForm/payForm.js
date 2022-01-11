9909070ed63da36d132643d72b3d08c06ea405e7
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-11 21:42:04 +0400

    feat: add a login check from the main page

--- src/components/payForm/payForm.js
+++ src/components/payForm/payForm.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Button, Container, Form} from "./payFormStyle";
 import InputBox from "../inputBox/inputBox";
+import GlobalContext from "../../context/context";
+import {useNavigate} from "react-router-dom";
 
 const PayForm = () => {
+    const {GlobalState, GlobalDispatch} = useContext(GlobalContext);
+    const {operator} = GlobalState;
+    const navigator = useNavigate();
+    useEffect(() => {
+        if (!operator.name) {
+            navigator('/')
+        }
+    }, [operator])
     return (
         <Form>
             <Container>
-                <img src="img/mts.svg" alt="mts" width={235} height={50}/>
+                <img src={operator.logo} alt={operator.name} width={235} height={50}/>
                 <InputBox type={'tel'} name={'phoneNumber'} placeHolder={'Номер телефона'} label={'Номер'}/>
                 <InputBox type={'tel'} name={'amountPay'} placeHolder={'Сумма оплаты'} label={'Сумма'}/>
                 <Button type={'submit'}>Оплатить</Button>
