f615fe38c99878a8a376030951990f3f95635d72
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-11 21:37:20 +0400

    fix: change func condition, update style for main

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {Header, List, ListItem, Title} from "./indexStyle";
+import {Header, List, ListItem, Title, Main} from "./indexStyle";
 import Wrapper from "../../components/wrapper/wrapper";
 import Operator from "../../components/operator/operator";
 import {getOperators} from "../../api/getOperators";
@@ -8,37 +8,37 @@ import {changeOperator} from "../../action/action";
 
 const Index = () => {
     const {GlobalDispatch} = useContext(GlobalContext);
-    const [operators,setOperators] = useState([])
+    const [operators, setOperators] = useState([])
     const fetchOperators = async () => {
         try {
             const data = await getOperators();
             const {operators} = await data;
             setOperators(operators)
-        } catch (e){
+        } catch (e) {
             console.error(e)
         }
     }
     const openForm = (id) => {
-        const selectedOperator = operators.find((operator) => operator.id = id)
+        const selectedOperator = operators.find((operator) => operator.id === id)
         GlobalDispatch(changeOperator(selectedOperator))
     }
-    useEffect(()=>{
+    useEffect(() => {
         fetchOperators()
-    },[])
+    }, [])
     return (
         <Wrapper>
             <Header>
                 <Title>Выберете оператора:</Title>
             </Header>
-            <main>
+            <Main>
                 <List>
-                    {operators.map((operator)=> (
+                    {operators.map((operator) => (
                         <ListItem key={operator.id}>
-                           <Operator name={operator.name} logo={operator.logo}/>
+                            <Operator name={operator.name} logo={operator.logo} onClick={() => openForm(operator.id)}/>
                         </ListItem>
                     ))}
                 </List>
-            </main>
+            </Main>
         </Wrapper>
     );
 };
--- src/page/index/indexStyle.js
+++ src/page/index/indexStyle.js
@@ -1,16 +1,23 @@
 import styled from "styled-components";
+import {DEKSTOP} from "../../constants/breakpoints";
 
 export const Header = styled.header`
   padding-top: 20px;
-  padding-bottom: 20px;
-`
+  padding-bottom: 20px;`
 export const Title = styled.h1`
   font-size: 2.0rem;
   line-height: 2.3rem;`
+export const Main = styled.main`
+  display: flex;
+  justify-content: center;`
 export const List = styled.ul`
   display: flex;
   gap: 20px;
   justify-content: center;
-  flex-wrap: wrap;`
+  flex-wrap: wrap;
+  @media (min-width: ${DEKSTOP}) {
+    max-width: 80%;
+    justify-content: center;
+  }`
 export const ListItem = styled.li`
   display: flex;`
