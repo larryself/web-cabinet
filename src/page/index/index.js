94e8e2dca18efc0f42fceb11e9c331122c254038
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-11 15:49:39 +0400

    refactor: add index page

---
+++ src/page/index/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Header} from "./indexStyle";
+
+const Index = () => {
+    return (
+        <React.Fragment>
+            <Header>
+                <h1>Выберете оператора:</h1>
+            </Header>
+        </React.Fragment>
+    );
+};
+
+export default Index;
---
+++ src/page/index/indexStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  padding-top: 20px;
+  padding-bottom: 20px;
+`
+export const Title = styled.h1`
+  font-size: 2.0rem;
+  line-height: 2.3rem;`
