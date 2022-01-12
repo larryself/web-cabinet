1d499c6c6d119fe31d14954b074d0655450db210
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-12 11:19:37 +0400

    fix: change amount mask

--- src/utils/maskValidate.js
+++ src/utils/maskValidate.js
@@ -2,24 +2,30 @@ const getInputNumbersValue = (value) => {
     return value.replace(/\D/g, '');
 }
 const amountValidation = (value) => {
-    return value.slice(0, 9).split('').map((num, i, arr) => {
-        if ((arr.length - 1 - i) % 3 === 0 && i !== arr.length - 1) {
+    const valueToNum = +value;
+    let formattedInputValue = "";
+    formattedInputValue = String(valueToNum).slice(0, 7);
+    return formattedInputValue.split('').map((num, index, arr) => {
+        if (arr[0] === '0') {
+            return ''
+        }
+        if ((arr.length - 1 - index) % 3 === 0 && index !== arr.length - 1) {
             return num + ' ';
         }
         return num;
-    }).join('');
+    }).join('')
 }
 const phoneValidation = (value) => {
     let formattedInputValue = "";
-    if(['7','8','9',].includes(value[0])){
-        if(value[0] === '9') formattedInputValue = `+7 (${value[0]}`;
-        if(value[0] === '7') formattedInputValue = `+7 (`;
-        if(value[0] === '8') formattedInputValue = `8 (`;
+    if (['7', '8', '9',].includes(value[0])) {
+        if (value[0] === '9') formattedInputValue = `+7 (${value[0]}`;
+        if (value[0] === '7') formattedInputValue = `+7 (`;
+        if (value[0] === '8') formattedInputValue = `8 (`;
     }
-    if(value.length > 1) formattedInputValue += value.slice(1,4)
-    if(value.length > 4) formattedInputValue += ') ' + value.slice(4,7) ;
-    if(value.length > 7) formattedInputValue += '-' + value.slice(7,9);
-    if(value.length > 9) formattedInputValue += '-' + value.slice(9,11) ;
+    if (value.length > 1) formattedInputValue += value.slice(1, 4)
+    if (value.length > 4) formattedInputValue += ') ' + value.slice(4, 7);
+    if (value.length > 7) formattedInputValue += '-' + value.slice(7, 9);
+    if (value.length > 9) formattedInputValue += '-' + value.slice(9, 11);
     return formattedInputValue;
 }
 const maskValidate = (event, name) => {
@@ -37,35 +43,13 @@ const maskValidate = (event, name) => {
             break;
         default:
             break;
-    };
-    if (input.value.length !== selectionStart && name === 'phoneNumber') {
-        input.value = formattedValue;
-        if([7,8,12,15].includes(selectionStart)){
-            if(selectionStart === 7 || selectionStart === 8 ) {
-                input.selectionStart = 9;
-                input.selectionEnd = 9;
-            }
-            if (selectionStart === 12) {
-                input.selectionStart = 13;
-                input.selectionEnd = 13;
-            }
-            if(selectionStart === 15) {
-                input.selectionStart = 16;
-                input.selectionEnd = 16;
-            }
-        } else {
-            input.selectionStart = selectionStart;
-            input.selectionEnd = selectionEnd;
-        }
-        return;
     }
-    if(input.value.length !== selectionStart && name === 'amountPay' ){
+    if (input.value.length !== selectionStart) {
         input.value = formattedValue;
-
-        return;
+        input.selectionStart = selectionStart;
+        input.selectionEnd = selectionEnd;
+    } else {
+        input.value = formattedValue;
     }
-    input.value = formattedValue;
-
-
 };
 export default maskValidate;
