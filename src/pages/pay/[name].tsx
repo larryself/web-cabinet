760037a039d54ec55fb11bee05071969cc17afff
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-18 16:11:23 +0400

    update getServerSideProps

--- src/pages/pay/[name].tsx
+++ src/pages/pay/[name].tsx
@@ -1,7 +1,22 @@
-import Wrapper from "../../components/wrapper/wrapper";
-import PayForm from "../../components/payForm/payForm";
-import { PayPageProps } from "../../types";
+import axios from 'axios';
+import { GetServerSideProps } from 'next';
+import Wrapper from '../../components/wrapper/wrapper';
+import PayForm from '../../components/payForm/payForm';
+import { PayPageProps } from '../../types';
+import { API_OPERATORS_LIST, BASE_URL } from '../../constants/url';
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  try {
+    const name = context.params?.name;
+    const response = await axios.get(`${BASE_URL}${API_OPERATORS_LIST}/${name}`);
+    const operator = await response.data;
+    return { props: { operator } };
+  } catch (e) {
+    return {
+      notFound: true,
+    };
+  }
+};
 
 const PayPage = ({ operator }: PayPageProps) => (
   <Wrapper>
