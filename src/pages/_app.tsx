f4f38d385d3c280951c5733e9d83b134bd9f669a
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-18 16:08:44 +0400

    add toaster

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import '../styles/index.css';
+import { Toaster } from 'react-hot-toast';
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'enabled') {
   require('../mocks');
@@ -13,6 +14,16 @@ const MyApp = ({ Component, pageProps }: AppProps) => (
       <meta charSet={'utf-8'} />
       <meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
     </Head>
+    <Toaster
+      position={'top-right'}
+      toastOptions={{
+        style: {
+          padding: '20px',
+          background: '#333',
+          color: '#fff',
+        },
+      }}
+    />
     <Component {...pageProps} />
   </>
 );
