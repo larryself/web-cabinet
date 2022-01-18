b531602c1f5c584e768757d54dc3fe39307bfef3
Yan Musikhin <yanmusikhin@gmail.com>
2022-01-18 16:01:28 +0400

    add types

---
+++ src/types.ts
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export type Operator = {
+  id: number | undefined;
+  name: string;
+  logo: string;
+};
+export type FormData = {
+  phoneNumber: string;
+  amountPay: string;
+};
+export type OperatorProps = {
+  logo: string;
+  name: string;
+};
+export type ImgProps = {
+  logo: string;
+  width: string;
+  height: string;
+  alt: string;
+};
+export type PayPageProps = {
+  operator: Operator;
+};
+export type WrapperProps = {
+  children: React.ReactNode;
+};
+export type InputProps = {
+  type: string;
+  name: string;
+  label: string;
+  placeholder: string;
+  className: string | undefined;
+  endValue?: string | null;
+};
+export type IndexProps = {
+  operators: [Operator];
+};
